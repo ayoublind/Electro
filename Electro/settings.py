8b6868de56b3e2beb7077af871a021fe0b113209
ayoblind <ayoblind@gmail.com>
2018-05-20 19:29:18 +0000

    fixing one prb git add *

--- Electro/settings.py
+++ Electro/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'..','www','static')
+STATIC_ROOT = os.path.join(BASE_DIR,'.','www','static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'..','www','media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'.','www','media')
 SITE_ID = 1 
 
